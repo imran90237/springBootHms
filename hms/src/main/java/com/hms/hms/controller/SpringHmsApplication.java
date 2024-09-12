f39b6e84b5b67e045715cce4460d1fc8a3b7b56d
Md. Imran Khan <imran90237@gmail.com>
2024-09-12 18:13:53 +0600

    database conncetion

---
+++ hms/src/main/java/com/hms/hms/controller/SpringHmsApplication.java
@@ -0,0 +1,14 @@
+package com.hms.hms.controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHmsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Test compilation for \"Spring HMS\"");
+
+		SpringApplication.run(SpringHmsApplication.class, args);
+	}
+
+}
